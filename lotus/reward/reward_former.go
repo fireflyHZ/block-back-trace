db808f4d30fa2cae2b47014ca9bbd5faaf7e1dbe
wang <wangchaopeng@fireflyminer.com>
2021-01-29 18:13:32 +0800

    f099132

--- lotus/reward/reward_former.go
+++ lotus/reward/reward_former.go
@@ -1325,7 +1325,7 @@ func TetsGetInfo() {
 		fmt.Printf("Error get chain head err:%+v\n", err)
 		return
 	}
-	minerAddr, _ := address.NewFromString("f088290")
+	minerAddr, _ := address.NewFromString("f099132")
 	p, _ := nodeApi.StateMinerPower(context.Background(), minerAddr, blocks.Key())
 	fmt.Printf("==========%+v\n", p)
 
