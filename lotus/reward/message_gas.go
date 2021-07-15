e9064287ded62a42d3b7fac02c64978ff10b3f23
wang <wangchaopeng@fireflyminer.com>
2021-07-15 10:42:54 +0800

    解决寻找不存在actor的bug

--- lotus/reward/message_gas.go
+++ lotus/reward/message_gas.go
@@ -20,6 +20,7 @@ import (
 	"profit-allocation/tool/bit"
 	"profit-allocation/tool/sync"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -705,10 +706,9 @@ func initWallet(tipset types.TipSetKey) error {
 		}
 		mi, err := client.Client.StateMinerInfo(ctx, minerAddr, tipset)
 		if err != nil {
-			if err.Error() == " actor not found" {
+			if strings.Contains(err.Error(), "actor not found") {
 				continue
 			}
-			msgLog.Errorf("-----%s-----", err.Error())
 			msgLog.Errorf("StateMinerInfo miner:%+v err:%+v", m, err)
 			return err
 		}
